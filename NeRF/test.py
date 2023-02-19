8b3bb39a3392f751c18fd411362cb786ced14ab2
yuran <yuranuestc@163.com>
2023-02-19 11:39:41 +0800

    latest change 0219

--- NeRF/test.py
+++ NeRF/test.py
@@ -4,6 +4,6 @@ import torch
 
 if __name__ == '__main__':
     a = torch.cat((torch.tensor([0]), torch.tensor([1, 2])))
-    a = a.unsqueeze(2)
+    a = a.unsqueeze(1)
     print(a)
     print(a.shape)
